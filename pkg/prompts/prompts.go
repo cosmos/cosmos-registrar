dff34335b0ee7a93c1447a104feba34321dd524e
Andrea Giacobino <no.andrea@gmail.com>
2021-02-04 15:57:53 +0100

    add selects and input with defaults to prompts
    adds more options to the prompts packages

--- pkg/prompts/prompts.go
+++ pkg/prompts/prompts.go
@@ -20,6 +20,10 @@ func Confirm(f, defaultAnswer string, v ...interface{}) bool {
 	if err != nil {
 		return false
 	}
+	result = strings.TrimSpace(result)
+	if result == "" {
+		result = strings.ToLower(defaultAnswer)
+	}
 	return strings.ToLower(strings.TrimSpace(result)) == "y"
 }
 
@@ -30,15 +34,29 @@ func InputRequired(q string, v ...interface{}) (string, error) {
 			err = errors.New("the answer cannot be empty")
 		}
 		return
-	}, q, v...)
+	}, "", q, v...)
 }
 
-func input(validator func(v string) error, q string, v ...interface{}) (res string, err error) {
+// InputOrDefault - ask user for mandatory input
+func InputOrDefault(defaultValue, q string, v ...interface{}) (string, error) {
+	return input(func(v string) (err error) {
+		if strings.TrimSpace(v) == "" {
+			err = errors.New("the answer cannot be empty")
+		}
+		return
+	}, defaultValue, q, v...)
+}
+
+func input(validator func(v string) error, defaultValue, q string, v ...interface{}) (res string, err error) {
 	prompt := promptui.Prompt{
 		Label:    fmt.Sprintf(q, v...),
 		Validate: validator,
+		Default:  defaultValue,
 	}
 	res, err = prompt.Run()
+	if strings.TrimSpace(res) == "" {
+		res = defaultValue
+	}
 	return
 }
 
@@ -50,4 +68,35 @@ func PrettyMap(data map[string]interface{}) {
 	}
 	sort.Sort(settings)
 	println(strings.Join(settings, "\n"))
+}
+
+type Option struct {
+	Label string
+	Func  func() error
+}
+
+func NewOption(label string, op func() error) Option {
+	return Option{
+		Label: label,
+		Func:  op,
+	}
+}
+
+func Select(q string, options ...Option) (err error) {
+
+	items := make([]string, len(options))
+	for i, o := range options {
+		items[i] = o.Label
+	}
+	prompt := promptui.Select{
+		Label: q,
+		Items: items,
+	}
+	index, _, err := prompt.Run()
+	if err != nil {
+		return
+	}
+	err = options[index].Func()
+	return
+
 }
