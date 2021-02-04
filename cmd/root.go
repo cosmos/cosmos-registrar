fe769f0fd80a3528ee2b088f383a2d81aae3103c
Andrea Giacobino <no.andrea@gmail.com>
2021-02-04 16:59:29 +0100

    feat: add main interactive menu

--- cmd/root.go
+++ cmd/root.go
@@ -17,16 +17,19 @@ limitations under the License.
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/tendermint/tendermint/libs/log"
 
 	registrar "github.com/jackzampolin/cosmos-registrar/pkg/config"
+	"github.com/jackzampolin/cosmos-registrar/pkg/prompts"
 )
 
 var (
 	cfgFile       string
+	debug         bool
 	config        *registrar.Config
 	logger        = log.NewTMLogger(log.NewSyncWriter(os.Stdout))
 	noInteraction = false
@@ -39,7 +42,7 @@ func init() {
 	rootCmd.SilenceUsage = true
 	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file")
 
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolVarP(&debug, "debug", "d", false, "Enable debug logging")
 	rootCmd.Flags().BoolVarP(&noInteraction, "no-interactive", "y", false, "Run commands non interactively")
 
 	rootCmd.AddCommand(
@@ -64,5 +67,43 @@ func Execute() {
 }
 
 func interact(cmd *cobra.Command, args []string) {
-	println("welcome")
+
+	for {
+		prompts.Select("what shall we do today?",
+			prompts.NewOption("Register a new ChainID", func() (err error) {
+				// ask use to create a fork first
+				println(`
+Good choice, following this process you will submit a 
+pull request to the chain IDs registry hosted on GitHub:
+` + config.RegistryRoot + `
+
+The first step is to create a fork of the registry using this link:
+` + fmt.Sprintf("%s/fork", config.RegistryRoot))
+
+				if ok := prompts.Confirm("Go ahead and confirm when you have done so", "Y"); !ok {
+					println("please create the fork before continuing")
+					return
+				}
+				println(`
+Next enter the rpc url for a node of your network  
+eg http://10.0.0.1:26657
+`)
+				// TODO add rpc address prompt
+				rpc, err := prompts.InputRequired("rpc address")
+
+				claim(cmd, []string{rpc})
+				return
+			}),
+			prompts.NewOption("Update a Chain ID you control", func() (err error) {
+				println("coming soon")
+				return
+			}),
+			prompts.NewOption("Exit", func() (err error) {
+				println("goodbye!")
+				os.Exit(0)
+				return
+			}),
+		)
+	}
+
 }
